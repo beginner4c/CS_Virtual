29dc46b6f820d1ad274510d04150db7fba15b6fa
beginner4c <100949417+beginner4c@users.noreply.github.com>
2022-04-25 18:49:21 +0900

    Add files via upload
    기존 그림판 만들기에서 마우스 우클릭을 누르면 popup이 뜨는 것을 구현했다
    문제점은 popup을 그림 종류에 따라 다른 걸 보이려 하는데 코드가 반복될 거 같다
    다음 차례에서 상속과 동적 바인딩을 통해서 해결해보려 한다

---
+++ 21_MainPopup/Figure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_MainPopup
+{
+    public abstract class Figure // 메인 폼에서 함수의 parameter로 활용하기 위해 public 선언
+    {
+        public Figure()
+        {
+
+        }
+        public abstract void draw(Graphics g, Pen pen);
+
+        public abstract void setXY2(int x, int y);
+    }
+}
---
+++ 21_MainPopup/Line.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_MainPopup
+{
+    class Line : TwoPointFigure
+    {
+        public Line(int x, int y) // constructor
+            : base(x, y) // 상위 클래스 constructor 호출
+        {
+        }
+
+        // 선을 그리는 함수
+        public override void draw(Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, _x1, _y1, _x2, _y2);
+        }
+    }
+}
